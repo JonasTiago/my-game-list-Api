3cb7efe9d3f00ab0a039d437b75f49bf73f2122b
Jonas Tiago <jonastiago95@gmail.com>
2023-01-22 15:03:38 -0300

    feat: schema games

---
+++ src/schemas/gameSchema.ts
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+const gameValidation = Joi.object({
+    name: Joi.string(),
+    platform:Joi.string().valid("xbox","ps","pc"),
+    genre:Joi.string(),
+    status:Joi.string().valid("playing", "finished"),
+    gameTime:Joi.number()
+})
+
+export {
+    gameValidation
+}
