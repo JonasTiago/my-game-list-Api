7781284b8929e1fdc3ddb146170e7eb26d1e0897
Jonas Tiago <jonastiago95@gmail.com>
2023-01-23 10:01:08 -0300

    feat: game controller

--- src/controllers/gamesController.ts
+++ src/controllers/gamesController.ts
@@ -1,24 +1,43 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import { prismaClient } from "../database/database.js";
 import { gameValidation } from "../schemas/gameSchema.js";
 
 type Game = {
     name: string,
-    platform:string,
-    genre:string,
-    status:string,
-    gameTime:number
+    platform: string,
+    genre: string,
+    status: string,
+    gameTime: number
 };
 
-async function createGame(req:Request, res:Response){
-    const game : Game = req.body
+async function createGame(req: Request, res: Response) {
+    const game: Game = req.body;
 
-    const {error} = gameValidation.validate(game, {abortEarly: false})
+    const { error } = gameValidation.validate(game, { abortEarly: false })
 
-    if(error) {
+    if (error) {
         return res.status(400).send(error.details.map(erro => erro.message))
     }
 
-    res.send(game) 
+    try {
+
+        await prismaClient.game.create({
+            data: {
+                name: game.name,
+                platform: game.platform,
+                genre: game.genre,
+                status: game.status,
+                gameTime: game.gameTime,
+            }
+        }
+        )
+
+        res.send(game)
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
 };
 
 
